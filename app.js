ff139d80457cafeab6388b6924787febee0d11f8
Zain Alwan Wima Irfani <zainalwan4@gmail.com>
2022-10-04 23:26:38 +0700

    Enable CORS.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
@@ -18,11 +19,13 @@ const graphqlRouter = require('./routes/graphql');
 const app = express();
 
 app.use(logger('combined'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.options('/graphql', cors());
 app.use('/graphql', graphqlRouter);
 
 module.exports = app;
